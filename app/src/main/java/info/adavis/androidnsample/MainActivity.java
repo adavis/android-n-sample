abb6baba7e70fcd256978abed0c31be42cb8166e
Annyce Davis <annyce.davis@gmail.com>
2016-03-17 14:44:04 -0400

    [+] initial commit

---
+++ app/src/main/java/info/adavis/androidnsample/MainActivity.java
@@ -0,0 +1,27 @@
+package info.adavis.androidnsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static EditText nameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        final Button submitButton = (Button) findViewById(R.id.button);
+
+        submitButton.setOnClickListener(
+                view -> Toast.makeText(this, String.format("Welcome to N, %s!",
+                            nameEditText.getText().toString()), Toast.LENGTH_LONG).show()
+        );
+    }
+
+}
