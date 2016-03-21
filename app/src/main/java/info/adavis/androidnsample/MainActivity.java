9a6e80619ea7468bf93f73b60d6e2d417e658cca
Annyce Davis <annyce.davis@gmail.com>
2016-03-21 17:41:03 -0400

    [-] remove static from edit text view

--- app/src/main/java/info/adavis/androidnsample/MainActivity.java
+++ app/src/main/java/info/adavis/androidnsample/MainActivity.java
@@ -2,13 +2,14 @@ package info.adavis.androidnsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static EditText nameEditText;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
